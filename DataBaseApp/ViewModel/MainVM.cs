17960c941b5df67406df5124b433afbb8936c4d0
maksimmer348@gmail.com <123456mb>
2021-05-04 14:50:15 +0300

    create project

---
+++ DataBaseApp/ViewModel/MainVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+//using DataBaseApp.Model;
+
+
+namespace DataBaseApp.ViewModel
+{
+    class MainVm
+    {
+
+    }
+}
